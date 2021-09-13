265155e035f7ea520c2eeb3a3e10425c290d899c
cauegraciolip <cauegracioli@outlook.com>
2021-09-13 00:14:10 -0300

    consumo de api com fetch

--- Fetch/app.js
+++ Fetch/app.js
@@ -1,30 +1,58 @@
-const valorMoeda = JSON.parse(localStorage.getItem('valorMoeda')) || []
-
-const url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
-
 const btn = document.querySelector("input[name=btn]")
+const select = document.querySelector('select[name=moeda]')
+
+const converter = (insert, value) => {
+    return insert * value
+}
+
+const transformArr = (nome, cotacao) => {
+    return {nome, cotacao}
+}
+
+const valor = []
+
+const showData = (element) => {
+
+    const input = document.getElementById('text').value
+
+    const selectValue = select.options[select.selectedIndex].value
+
+    let valores
+
+    for ( let i in element ) {
+        valores = transformArr(element[i].code, JSON.parse((element[i].high)))
+        valor.push(valores)
+    }
+
+    let resultado = (index) => {
+        document.getElementById('content').innerHTML = `R$${input} = ${valor[index].nome} ${converter(input, valor[index].cotacao).toFixed(2)}`
+    }
+
+    if ( selectValue == "USD" ) {
+        resultado(0)
+    } else if (selectValue == "EUR") {
+        resultado(1)
+    } else {
+        resultado(2)
+    }
+}
 
 btn.addEventListener('click', () => {
-    let input = document.getElementById('text')
-    const inputValue = input.value
+
+    getAPI("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(console.log).catch(console.error)
+
 })
 
-const getValue = () => {
-
-    fetch(url)
-    .then((res) => {
-        return res.json()
-    })
-    .then((data) => {
-        for ( i in data ) {
-            valorMoeda.push(localStorage.setItem('valorMoeda', data[i].ask))
-        }
-    })
-}
-
-const converter = (valor, moeda) => {
-    return valor * moeda
-}
-
-
-
+const getAPI = (url) => {
+    const promiseCB = (reject) => {
+        fetch(url)
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                showData(data)
+            })
+            .catch(reject)
+    }
+    return new Promise(promiseCB)
+} 
